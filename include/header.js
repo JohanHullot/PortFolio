a2b3afe2975306ee3cde92e5ea0bfdf45c732e6d
JohanHullot <johan.hullot@gmail.com>
2025-03-02 20:26:34 +0100

    Design et remise à niveau

--- include/header.js
+++ include/header.js
@@ -7,7 +7,9 @@ document.getElementById("myNav").innerHTML =
             + "<button onclick='window.location.href = `home.html`;' class='box h'>Home</button>"
             + "<button onclick='window.location.href = `projets.html`;' class='box h'>Projets</button>"
             + "<button onclick='window.location.href = `parcours.html`;' class='box h'>Parcours</button>"
-            + "<button onclick='window.location.href = `contact.html` ;' class='box h'>Contact</button>"
+            + "<button onclick='window.location.href = `contact.html` ;' class='box h'>Contact et CV</button>"
         + "</div>"
+     
     + "</div>"
-    + "<hr/>";
+    + "<hr class = 'hrHeader' />"
+;
