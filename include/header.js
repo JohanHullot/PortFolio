994f81d6dc3dfa5b15f9ce606ad9fd1c0435bbc1
JohanHullot <johan.hullot@gmail.com>
2025-03-08 20:30:39 +0100

    up project page

--- include/header.js
+++ include/header.js
@@ -3,7 +3,7 @@ document.getElementById("myNav").innerHTML =
     "<div class='container'>"
 
     + "<div class='all'>"
-        + "<div class='box'>Johan Hullot  - </div>"
+            + "<button onclick='window.location.href = `parcours.html`;' class='box j'>Johan Hullot -</button>"
             + "<button onclick='window.location.href = `home.html`;' class='box h'>Home</button>"
             + "<button onclick='window.location.href = `projets.html`;' class='box h'>Projets</button>"
             + "<button onclick='window.location.href = `parcours.html`;' class='box h'>Parcours</button>"
